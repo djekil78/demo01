b974df519193f4fe2189d4db47e74b0062b5328b
Dmitry Chernushenko <40021138+djekil78@users.noreply.github.com>
2020-03-05 15:17:16 +0300

    Delete PersonGroupPersonRepository.java

--- src/main/java/ru/abcconsulting/bio/integration/repository/wfm/PersonGroupPersonRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.abcconsulting.bio.integration.repository.bio;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
-
-import ru.abcconsulting.bio.integration.entity.bio.PersonGroupPerson;
-
-public interface PersonGroupPersonRepository extends JpaRepository<PersonGroupPerson, Long>, QuerydslPredicateExecutor<PersonGroupPerson> {
-}
