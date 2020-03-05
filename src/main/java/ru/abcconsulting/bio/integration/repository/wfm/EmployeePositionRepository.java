9f45b3b5bdbacca4e5b24c1de778e18ea46833a3
Dmitry Chernushenko <40021138+djekil78@users.noreply.github.com>
2020-03-05 15:13:24 +0300

    Delete EmployeePositionRepository.java

--- src/main/java/ru/abcconsulting/bio/integration/repository/wfm/EmployeePositionRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.abcconsulting.bio.integration.repository.wfm;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ru.abcconsulting.bio.integration.entity.wfm.EmployeePosition;
-
-public interface EmployeePositionRepository extends JpaRepository<EmployeePosition, Long> {
-}
