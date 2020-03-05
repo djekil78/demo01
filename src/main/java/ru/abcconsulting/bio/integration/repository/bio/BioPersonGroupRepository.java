8506c6393b04c63f003f891013042f7d8681be7f
Dmitry Chernushenko <40021138+djekil78@users.noreply.github.com>
2020-03-05 15:08:16 +0300

    Create BioPersonGroupRepository.java

---
+++ src/main/java/ru/abcconsulting/bio/integration/repository/bio/BioPersonGroupRepository.java
@@ -0,0 +1,9 @@
+package ru.abcconsulting.bio.integration.repository.bio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+import ru.abcconsulting.bio.integration.entity.bio.BioPersonGroup;
+
+public interface BioPersonGroupRepository extends JpaRepository<BioPersonGroup, Long>, QuerydslPredicateExecutor<BioPersonGroup> {
+}
